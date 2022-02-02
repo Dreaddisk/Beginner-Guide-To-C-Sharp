c64f62f0b9b2dda66b3d6025cb04be475de619f2
Jose M. Puebla <joey_manuel@outlook.com>
2022-02-01 19:19:14 -0500

    1 Introduction to Variables
    Created the first file from the first example of the lecture.

---
+++ 1. Variables/Variables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int aVal = 7;
+            Console.WriteLine("My number is {0} and {1}", number, aVal);
+        }
+    }
+}
