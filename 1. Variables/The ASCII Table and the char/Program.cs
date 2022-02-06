45693d60fd4c8f14ef5d55e90508c7683653a0fd
Jose M. Puebla <joey_manuel@outlook.com>
2022-02-06 09:18:41 -0500

    3 Created the 3rd Exercise
    ASCII and char explanation.

---
+++ 1. Variables/The ASCII Table and the char/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Variables
+{
+    static public void Main()
+    {
+        int number = 'A';
+        char myCharacter = 'A';
+        char _myCharacter = (char) 65;  // typecasting method.
+        Console.WriteLine("My value is {0} and {1}", number, _myCharacter);
+    }
+}
