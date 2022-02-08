6ee2a194811729f73a5b516995bae8151d1f0384
Jose M. Puebla <joey_manuel@outlook.com>
2022-02-08 18:07:05 -0500

    6 A Qord about Formatting

---
+++ 1. Variables/A Word About Formatting/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class FormattingMyCode
+{
+    static public void Main()
+    {
+        int x = 5;
+        float y =  8f;
+        char myChar =  'a';
+        string name = "My name is kjnfkjnjnkjsdk"+
+        "dEJFNqWefnfkncKFNKeasdRB"+"werKBSFDBKxcvrrh"+
+        "bjlhblklnkjvg";
+    }
+}
